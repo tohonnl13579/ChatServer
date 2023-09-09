4280c28407441a341cf13c15e908723297d91661
Ariel Starling <arielstarling25@gmail.com>
2023-09-09 23:30:00 +0800

    Added Functionality to switch Windows

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -22,11 +22,17 @@ namespace ClientWPF
     {
         private DataServerInterface foob;
         private ChannelFactory<DataServerInterface> foobFactory;
+        private string loggedUser;
         public MainWindow()
         {
             InitializeComponent();
+
             Warning_Label.Content = "";
             Username_TextBox.Text = "";
+            LoggedIn_Label.Content = "Logged in as: ";
+            Button_EnterChatRoom.Visibility = Visibility.Hidden;
+            Button_EnterChatRoom.IsEnabled = false; 
+
             connectToServer();
         }
 
@@ -41,6 +47,60 @@ namespace ClientWPF
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             //Button Functionality
+            Warning_Label.Content = "";
+            string username = Username_TextBox.Text;
+            if (username.Length == 0)
+            {
+                Warning_Label.Content = "Username field should not be empty!";
+            }
+            else
+            {
+                bool logged = foob.addUser(username);
+                if (logged)
+                {
+                    Warning_Label.Content = "Logged in successfully!";
+                    loggedUser = username;
+                    LoggedIn_Label.Content = "Logged in as: " + loggedUser;
+                    Button_EnterChatRoom.Visibility= Visibility.Visible;
+                    Button_EnterChatRoom.IsEnabled = true;
+                }
+                else
+                {
+                    Warning_Label.Content = "User exists!";
+                }
+            }
+        }
+
+        private void enterChatRoom()
+        {
+            ChatRoomWindow chatRoomWindow = new ChatRoomWindow(loggedUser);
+            if (chatRoomWindow != null) { 
+                chatRoomWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                Warning_Label.Content = "Failed to switch windows";
+            }
+        }
+
+        private void EnterChatRoom_Click(object sender, RoutedEventArgs e)
+        {
+            //Button Func
+            //Attempt to close the connection, so the other window can connect
+            try
+            {
+                ((ICommunicationObject)foob).Close();
+                foobFactory.Close();
+                //Enter chatroom window
+                enterChatRoom();
+            }
+            catch (Exception eR)
+            {
+                Console.WriteLine(eR.Message);
+                Warning_Label.Content = eR.Message;
+                connectToServer();
+            }
         }
     }
 }
