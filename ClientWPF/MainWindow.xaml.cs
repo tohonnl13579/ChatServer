245b15949be536d41f97b9c124ef1c7ac50a7e03
Ariel Starling <arielstarling25@gmail.com>
2023-09-09 22:48:47 +0800

    New Branch Created: Client Side Work

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -18,9 +18,6 @@ using System.Windows.Shapes;
 
 namespace ClientWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private DataServerInterface foob;
