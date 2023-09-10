e497c158b9e5dad9af2bc31865718e9fe563a2d5
Ariel Starling <arielstarling25@gmail.com>
2023-09-10 12:55:09 +0800

    Added updating logic and did some modification to GUI

--- ClientWPF/ChatRoomWindow.xaml.cs
+++ ClientWPF/ChatRoomWindow.xaml.cs
@@ -21,22 +21,101 @@ namespace ClientWPF
     {
         private DataServerInterface foob;
         private ChannelFactory<DataServerInterface> foobFactory;
-        private string loggedUser;
+        private string loggedUser, currChatRoom;
+        private int maxConnectAtt;
         public ChatRoomWindow(string user)
         {
             InitializeComponent();
             loggedUser = user;
+            maxConnectAtt = 6;
             ChatRoomWarning_Label.Content = "";
             connectToServer();
+            updateRooms();
         }
 
-        private void connectToServer()
+        private bool connectToServer()
         {
+            bool connect = true;
             NetTcpBinding tcpB = new NetTcpBinding();
             string URL = "net.tcp://localhost:8100/DataService";
             foobFactory = new ChannelFactory<DataServerInterface>(tcpB, URL);
             foob = foobFactory.CreateChannel();
-            ChatRoomWarning_Label.Content = loggedUser;
+            Label_LoggedAs.Content = "Logged as: " + loggedUser;
+
+            //Once it reaches maximum connection attempts, client fails to connect to server.
+            if(maxConnectAtt <= 0)
+            {
+                connect = false;
+            }
+            else
+            {
+                maxConnectAtt--;
+            }
+
+            return connect;
+        }
+
+        //Event Handling
+
+
+        //Updating GUI Lists
+        private void updateRooms()
+        {
+            ChatRoomWarning_Label.Content = "";
+            ListBox_RoomList.Items.Clear();
+            try
+            {
+                List<string> roomList = foob.GetChatRoomList();
+                for(int i = 0; i < roomList.Count; i++)
+                {
+                    ListBoxItem item = new ListBoxItem();
+                    item.Content = roomList[i];
+                    ListBox_RoomList.Items.Add(item);
+                }
+            }
+            catch (CommunicationException cE)
+            {
+                ChatRoomWarning_Label.Content = "Connection Lost!: " + cE.Message;
+                connectToServer();
+            }
+            catch (Exception eR)
+            {
+                ChatRoomWarning_Label.Content = "Exception occured: " + eR.Message;
+            }
+
+        }
+
+        private void updateMessages()
+        {
+            ChatRoomWarning_Label.Content = "";
+        }
+
+        private void updateUsers()
+        {
+            ChatRoomWarning_Label.Content = "";
+            try
+            {
+                HashSet<string> userOnline = foob.GetUserOnline(currChatRoom);
+                ListBox_UserList.Items.Clear();
+                if (userOnline.Count > 0)
+                {
+                    foreach(string user in userOnline)
+                    {
+                        ListBoxItem item = new ListBoxItem();
+                        item.Content = user;
+                        ListBox_UserList.Items.Add(user);
+                    }
+                }
+            }
+            catch (CommunicationException cE)
+            {
+                ChatRoomWarning_Label.Content = "Connection Lost!: " + cE.Message;
+                connectToServer();
+            }
+            catch (Exception eR)
+            {
+                ChatRoomWarning_Label.Content = "Exception occured: " + eR.Message;
+            }
         }
     }
 }
